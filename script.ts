763777fd03e0f30aaf281e488be7809fe7bf5c15
a.mizerny <a.mizerny@kryptonite.ru>
2026-02-12 01:20:38 +0300

    codestyle

--- script.ts
+++ script.ts
@@ -8,26 +8,22 @@ import ScrollTrigger from "gsap/ScrollTrigger";
 history.scrollRestoration = "manual";
 
 gsap.registerPlugin(Observer, ScrollToPlugin, ScrollTrigger);
-gsap.config({
-  force3D: true
-});
 
-const sectionsContainer = document.querySelector('.main-page');
-let sections = Array.from(sectionsContainer!.querySelectorAll(".animate-show-block"));
+const sectionsContainer = document.querySelector(".main-page");
+let sections = Array.from(
+    sectionsContainer!.querySelectorAll(".animate-show-block"),
+);
 let stepsTops: { top: number; element?: Element }[] = sections.flatMap((el) => {
-	const top = el.getBoundingClientRect().top;
+    const top = el.getBoundingClientRect().top;
 
-	return [
-		{ top, element: el },
-		{ top: top + 100 }
-	]
-})
+    return [{ top, element: el }, { top: top + 100 }];
+});
 
 let currentIndex = 0;
 
 let isAnimating = false;
 
-function goToSection(index: number, direction: 'up' | 'down') {
+function goToSection(index: number, direction: "up" | "down") {
     if (isAnimating) return;
 
     if (index < 0 || index >= stepsTops.length) return;
@@ -36,14 +32,14 @@ function goToSection(index: number, direction: 'up' | 'down') {
 
     isAnimating = true;
 
-	const el = stepsTops[index].element;
-	let duration: number = 1;
-	if (direction === 'up') {
-		duration = stepsTops[index].element ? 1 : 1;
-	}
-	if (direction === 'down') {
-		duration = stepsTops[index + 1].element ? 1 : 1;
-	}
+    const el = stepsTops[index].element;
+    let duration: number = 1;
+    if (direction === "up") {
+        duration = stepsTops[index].element ? 1 : 1;
+    }
+    if (direction === "down") {
+        duration = stepsTops[index + 1].element ? 1 : 1;
+    }
 
     gsap.to(sectionsContainer, {
         scrollTo: {
@@ -52,27 +48,27 @@ function goToSection(index: number, direction: 'up' | 'down') {
         duration,
         ease: "power1.inOut",
         onComplete: () => {
-            setTimeout(() => isAnimating = false, 900);
+            setTimeout(() => (isAnimating = false), 900);
         },
     });
 
-	if (direction === 'down') {
-		const prevEl = stepsTops[index + 1].element;
-		if (prevEl) {
-			prevEl.classList.remove('show');
-		};
-		if (el) {
-			el.classList.remove('with-filter')
-		}
-	} 
-	if (direction === 'up') {
-		if (el) {
-			el.classList.add('show');
-		} else {
-			const prevEl = stepsTops[index - 1].element;
-			prevEl?.classList.add('with-filter')
-		}
-	}
+    if (direction === "down") {
+        const prevEl = stepsTops[index + 1].element;
+        if (prevEl) {
+            prevEl.classList.remove("show");
+        }
+        if (el) {
+            el.classList.remove("with-filter");
+        }
+    }
+    if (direction === "up") {
+        if (el) {
+            el.classList.add("show");
+        } else {
+            const prevEl = stepsTops[index - 1].element;
+            prevEl?.classList.add("with-filter");
+        }
+    }
 }
 
 Observer.create({
@@ -83,11 +79,10 @@ Observer.create({
     preventDefault: true,
 
     onDown() {
-        goToSection(currentIndex - 1, 'down');
+        goToSection(currentIndex - 1, "down");
     },
 
     onUp() {
-        goToSection(currentIndex + 1, 'up');
+        goToSection(currentIndex + 1, "up");
     },
 });
-
